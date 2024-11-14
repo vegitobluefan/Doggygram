2e437f3dde6aaf61d968335a39928842fa35294e
vegitobluefan <daniararinov995@gmail.com>
2024-11-14 19:23:51 +0300

    сделал .env файл

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -1,13 +1,18 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-4p1rl$3d#a$vbhhyk%aj9sez^*uapcdv!kvph($uk$2ojwpmd9'
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = True
+DEBUG = (os.getenv('DEBUG', default='False') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
